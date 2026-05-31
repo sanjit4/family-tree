bb0e1cb5a9509a00dca1d5c82d28b162bee2af41
Sanjit <sanjitgurung94@gmail.com>
2026-05-31 22:17:56 +0100

    update view_family and view_relationship

--- main.py
+++ main.py
@@ -14,24 +14,33 @@ class Person:
         self.relationships = []
 
     def make_relationship(self, relationship_type:str, other_person = None, start_date = None, end_date = None):
-        relationship = {other_person: other_person, relationship_type: relationship_type, start_date: start_date, end_date: end_date}
+        relationship = {'other_person': other_person, 'relationship_type': relationship_type, 'start_date': start_date, 'end_date': end_date}
         self.relationships.append(relationship)
     
-    def view_relationships(self):
+    def view_family(self):
         return self.relationships
+    
+    def view_relationship(self, other_person):
+        for relationship in self.relationships:
+            # print(relationship.get(other_person))
+            # if relationship.get(other_person) == other_person:
+            if relationship.get('other_person') == other_person:
+                return relationship
+        return None
 
 
-# class Family:
-#     def make_relationship(self, other_person):
-#         # This is a placeholder for a method that would determine the relationship between two people
-#         pass
+def main():
+    person1 = Person(first_name="John", last_name="Doe", date_of_birth=date(1990, 1, 1))
+    person2 = Person(first_name="Jane", last_name="Smith", date_of_birth=date(1992, 2, 2))
+    person3 = Person(first_name="Janice", last_name="Doe", date_of_birth=date(2020, 3, 3))
+
+    person1.make_relationship("husband", person2, start_date=date(2010, 1, 1))
+    person1.make_relationship("daughter", person3)
+
+    # print(person1.view_close_relationships())
+    # print(person1.view_relationship(person2))
+    print(person1.view_relationship(person3))
 
 
 if __name__ == "__main__":
-    person1 = Person(first_name="John", last_name="Doe", date_of_birth=date(1990, 1, 1))
-    
-    person2 = Person(first_name="Jane", last_name="Smith", date_of_birth=date(1992, 2, 2))
-
-    person1.make_relationship("friend", person2, start_date=date(2010, 1, 1))
-
-    print(person1.view_relationships())
+    main()
